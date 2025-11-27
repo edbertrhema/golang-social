64fa3c4aecaf0ed15992c7ccd8132742598384c3
edbert-pci <edbert.adisetya@praweda.id>
2025-11-27 09:37:58 +0700

    add: repo logic in user and post

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -3,6 +3,8 @@ package store
 import (
 	"context"
 	"database/sql"
+
+	"github.com/lib/pq"
 )
 
 type Post struct {
@@ -15,12 +17,17 @@ type Post struct {
 	UpdateAt  string   `json: "update_at"`
 }
 
-type PostsStore struct {
+type PostStore struct {
 	db *sql.DB
 }
 
-func (s *PostsStore) Create(ctx context.Context, post *Post) error {
+func (s *PostStore) Create(ctx context.Context, post *Post) error {
 	query := `INSERT INTO posts (content, title, user_id, tags) VALUES ($1, $2, $3, $4) RETURNING id, created_at, update_at`
 
-	s.db.QueryContext(ctx, query, post.Content, post.Title, post.UserID, post.Tags)
+	err := s.db.QueryRowContext(ctx, query, post.Content, post.Title, post.UserID, pq.Array(post.Tags)).Scan(&post.ID, &post.CreatedAt, &post.UpdateAt)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
