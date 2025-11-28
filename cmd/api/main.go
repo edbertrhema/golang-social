5fc0ba42511bef60bdc70c22f3836f366af16d6c
edbert-pci <edbert.adisetya@praweda.id>
2025-11-28 14:23:09 +0700

    add: postgress db pool

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"social/internal/db"
 	"social/internal/env"
 	"social/internal/store"
 )
@@ -9,8 +10,21 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8085"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgresql://postgres:admin@localhost:5432/postgres?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
-	s := store.NewStorage(nil)
+
+	database, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Println("Connected to database")
+	s := store.NewStorage(database)
 	app := &application{
 		config: cfg,
 		store:  s,
